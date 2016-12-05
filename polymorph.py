a7c6599b602a3e109f1b40a3a5aeb3ad88b198d3
sleepy <williamvanskike@gmail.com>
2016-12-04 21:21:53 -0800

    I made a program that rewrites itself called polymorph.py.. Ignore the crappy poetry..

---
+++ polymorph.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import os
+
+filePtr = open('.//polymorph.py', 'r')
+fileContent = filePtr.readlines()
+
+runFlag = 1
+
+poemB = "I am always changing, a growing tree or a dulled tool, tedious, exhausting, I continue.\nI can become a new job to be rebuilt, a task with no solution. What was old is new and what was new is old,\nWe forget our face while you forget my name."
+poemA = "I am never the same at any instance in time, I am different every time I run, no matter how far I run.\nI can be resewn and rewritten while I do my work, I can be rebuilt to become a new job,\nwhose collar was once blue is now white, what skin that was once smooth is now hard.."
+print(poemB)
+
+if runFlag == 0:
+    fileContent[6] = "runFlag = 1\n"
+    fileContent[10] = "print(poemB)\n"
+else:
+    fileContent[6] = "runFlag = 0\n"
+    fileContent[10] = "print(poemA)\n"
+
+filePtr = open('.//polymorph.py', 'w')
+filePtr.writelines(fileContent)
+filePtr.close()
+
+#This is a program that rewrites itself after each run to display a new poem.
+# I got the idea from a book about reverse engineering
